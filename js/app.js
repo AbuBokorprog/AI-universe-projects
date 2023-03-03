bbd71d6637fa82fba64709fc9cc676e196303a81
Abu Bokor <abubokor1066@gmail.com>
2023-03-03 13:01:45 +0600

    title and date added

--- js/app.js
+++ js/app.js
@@ -10,27 +10,39 @@ const displayCard = (data) => {
   const cardContainer = document.getElementById("card-container");
   data.forEach((card) => {
     //console.log(card);
-    card.features.forEach((list) => {
+    /* card.features.forEach((list) => {
       console.log(list);
-    });
+    }); */
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
     <div class="card h-100">
     <img src="${card.image}" class="card-img-top rounded-5 p-3" alt="" />
     <div class="card-body">
-      <h5 class="card-title">Features</h5>
-      <ol>
-      <li></li>
-      </ol>
+      <h5 id="feature" class="card-title">Features</h5>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+      <h5>${card.name}</h5>
+      <small class="text-muted"><i class="fa-regular fa-calendar"></i>${card.published_in}</small>
+      <button><i class="fa-sharp fa-solid fa-arrow-right text-right"></i></button>
     </div>
   </div>
     `;
     cardContainer.appendChild(div);
   });
+  /* const feature = document.getElementById("feature");
+  //const ol = document.createElement("ol");
+  data.forEach((card) => {
+    //console.log(card);
+    card.features.forEach((list) => {
+      console.log(list);
+      feature.innerHTML = `
+      <ol>
+      <li>${list}</li>
+      </ol>
+      `;
+    });
+  }); */
 };
 
 loadData();
